ddf131afe3ae31556ab73a592a1ec7d56c0a3986
abinsuwal <abinsuwal650@gmail.com>
2026-01-09 21:27:44 +0545

    restarted project to fix project format

--- accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('login', views.Login, name='login'),
-    path('register', views.Register, name='register'),
-    path('logout', views.Logout, name='logout'),
-    
-]
--- products/admin.py
+++
@@ -1,7 +0,0 @@
-
-# Register your models here.
-from django.contrib import admin
-from .models import Product
-
-# Register your Product model
-admin.site.register(Product)
