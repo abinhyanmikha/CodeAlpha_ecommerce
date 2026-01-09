b46fe945b23e370d3eda7a704b37296adb14cabb
abinsuwal <abinsuwal650@gmail.com>
2026-01-09 21:54:15 +0545

    setup of views part

--- ecommerce/main/models.py
+++ ecommerce/main/models.py
@@ -1,3 +1,24 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price=models.FloatField()
+    description=models.TextField()
+    image=models.ImageField(upload_to='products/')
+    stock=models.IntegerField()
+    category=models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    #if user delete account all orders deleted automatically
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    total_price=models.FloatField()
+    created_at=models.DateTimeField(auto_now_add=True)
+
+class OrderItem(models.Model):
+    order=models.ForeignKey(Order,on_delete=models.CASCADE)
+    product=models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity=models.IntegerField()
--- ecommerce/main/views.py
+++ ecommerce/main/views.py
@@ -1,3 +1,77 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from .models import Product,Order,OrderItem
+
+
 
 # Create your views here.
+def home(request):
+    products=Product.objects.all()
+    return render(request,'home.html',{'products':products})
+
+def product_detail(request,product_id):
+    product=Product.objects.get(id=product_id)
+    return render(request,'product.html',{'product':product})
+
+def add_to_cart(request,product_id):
+    cart=request.session.get('cart',{})
+    cart[str(id)]=cart.get(str(id),0)+1
+    request.session['cart']=cart
+    return redirect('cart')
+
+
+def cart(request):
+    cart=request.session.get('cart',{})
+    items=[]
+    total=0
+
+    for pid,quantity in cart.items():
+      product = Product.objects.get(id=pid)
+      subtotal = product.price * quantity
+      total += subtotal
+      items.append({'product': product, 'quantity': quantity, 'subtotal': subtotal})
+    return render(request,'cart.html',{'items':items,'total':total})
+
+@login_required
+def checkout(request):
+    cart=request.session.get('cart',{})
+    if not cart:
+        return redirect('home')
+    total=0
+    order=Order,objects.create(user=request.user, total_price=0)
+    for pid, quantity in cart,items():
+        product=Product.objects.get(id=id)
+        OrderItem.objects.create(order=order,product=product,quantity=quantity)
+        total+=product.price*quantity
+    order.total_price=total
+    order.save()
+    request.session['cart']={}
+    return render(request,'success.html',{'order':order})
+
+def register(request):
+    if request.method=='POST':
+        User.object.create_user(
+            username=request.POST['username'],
+            password=request.POST['password'],
+            email=request.POST['email'],
+        )
+        return redirect('login')
+    return render(request,'register.html')
+
+def login(request):
+    if request.method=='POST':
+        user=authenticate(
+            username=request.POST['username'],
+            password=request.POST['password'],
+        )
+        if user:
+            login(request,user)
+            return redirect('home')
+        else:
+            return render(request,'login.html',{'error':'Invalid Credentials'})
+    return render(request,'login.html')
+    
+
+def logout(request):
+    logout(request)
+    return redirect('home')
